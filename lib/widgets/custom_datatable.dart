1bd359dbea9df10fa6bdfbac33225a6b06cfae2c
Sanjar17 <sanjarsaidov173@gmail.com>
2023-06-24 20:09:21 +0500

    vertical and horizontal is working

--- lib/widgets/custom_datatable.dart
+++ lib/widgets/custom_datatable.dart
@@ -1,43 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:scrollable_table_view/scrollable_table_view.dart';
 
 List<String> columnsList = ['N~', 'Savdo raqami', 'Holati', 'Mijozlar', 'Sana', 'Jami', 'SUM'];
 
 List<Map> rowsList = [
-  {'id': 1, 'savdo_raqami': 'Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
-  {'id': 2, 'savdo_raqami': 'Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
-  {'id': 3, 'savdo_raqami': 'Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
-  {'id': 4, 'savdo_raqami': 'Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
-  {'id': 5, 'savdo_raqami': 'Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
-  {'id': 6, 'savdo_raqami': 'Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
-  {'id': 7, 'savdo_raqami': 'Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
-  {'id': 8, 'savdo_raqami': 'Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
+  {'id': 2, 'savdo_raqami': '1Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
+  {'id': 1, 'savdo_raqami': '2Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
+  {'id': 3, 'savdo_raqami': '3Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
+  {'id': 4, 'savdo_raqami': '4Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
+  {'id': 5, 'savdo_raqami': '5Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
+  {'id': 6, 'savdo_raqami': '6Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
+  {'id': 7, 'savdo_raqami': '7Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
+  {'id': 8, 'savdo_raqami': '8Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
   {'id': 9, 'savdo_raqami': 'Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
   {'id': 10, 'savdo_raqami': 'Savdo N 18', 'Holati': 'O\'tkazilgan', 'mijoz': 'Klient', 'sana': '22/06/2023 06:53', 'JAMI': '3 000.00', 'sum': '34 000.00'},
 ];
 
-class CustomDataTable extends StatelessWidget {
+class CustomDataTable extends StatefulWidget {
   const CustomDataTable({
     super.key,
   });
 
-  List<TableViewColumn> getColumns(List<String> columns) => columns.map((e) => TableViewColumn(label: e)).toList();
+  @override
+  State<CustomDataTable> createState() => _CustomDataTableState();
+}
 
-  List<TableViewRow> getRows(List<Map> rows) => rows.map((e) => TableViewRow(cells: getCells(e))).toList();
+class _CustomDataTableState extends State<CustomDataTable> {
+  int? currentSortColumn;
 
-  List<TableViewCell> getCells(Map row) => row.entries.map((e) => TableViewCell(child: Text(e.value.toString()))).toList();
+  bool? currentSortAscending;
+
+  List<DataColumn> getColumns(List<String> columns) => columns.map((e) => DataColumn(label: Expanded(child: Text(e)), onSort: onSort)).toList();
+
+  List<DataRow> getRows(List<Map> rows) => rows.map((e) => DataRow(cells: getCells(e))).toList();
+
+  List<DataCell> getCells(Map row) => row.values.map((e) => DataCell(Text(e.toString()))).toList();
+
+  void onSort(columnIndex, ascending) {
+    if (ascending) {
+      rowsList.sort((rowA, rowB) => rowA.values.toList()[columnIndex].compareTo(rowB.values.toList()[columnIndex]));
+    } else {
+      rowsList.sort((rowA, rowB) => rowB.values.toList()[columnIndex].compareTo(rowA.values.toList()[columnIndex]));
+    }
+
+    currentSortColumn = columnIndex;
+    currentSortAscending = ascending;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     Size constraints = MediaQuery.of(context).size;
 
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: ConstrainedBox(
-        constraints: BoxConstraints(minWidth: constraints.width),
-        child: ScrollableTableView(
-          columns: getColumns(columnsList),
-          rows: getRows(rowsList),
+    return Expanded(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minWidth: constraints.width),
+            child: DataTable(
+              sortColumnIndex: currentSortColumn,
+              sortAscending: currentSortAscending ?? true,
+              columns: getColumns(columnsList),
+              rows: getRows(rowsList),
+            ),
+          ),
         ),
       ),
     );
